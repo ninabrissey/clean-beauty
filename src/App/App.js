ab192cb2744d1f3c226f35e9cc1d1d967ebf20b8
Nina Brissey <nbrissey@gmail.com>
2021-09-23 20:10:29 -0700

    Add state, fetch, and return

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { fetchData } from '../apiCalls';
 import Nav from '../Nav/Nav';
 import './App.css';
 
 const App = () => {
+  const [eyes, setEyes] = useState([]);
+  const [face, setFace] = useState([]);
+  const [lips, setLips] = useState([]);
+  const [cheeks, setCheeks] = useState([]);
+  const [nails, setNails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const handleClick = () => {};
+
+  const getData = (path) => {
+    try {
+      fetchData(path);
+    } catch {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="app">
-      <Nav />
+      <Switch>
+        <Nav />
+        <Route to="/">
+          <HomePage />
+        </Route>
+        <Route to="/:id">
+          <MakeupContainer />
+        </Route>
+      </Switch>
     </div>
   );
 };
