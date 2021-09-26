c0246f5426e8dd17da8cbba8d61ce362c6ef83a1
Nina Brissey <nbrissey@gmail.com>
2021-09-25 17:14:46 -0700

    Add additional makeup categories

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -9,6 +9,16 @@ const Nav = ({ handleChange }) => {
       <img className="nav-logo" src={logo} alt="clean beauty logo" />
       <ul className="nav-links">
         <li>
+          <NavLink to="/categories/blush" className="nav-link" id="blush">
+            blush
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories/bronzer" className="nav-link" id="bronzer">
+            bronzer
+          </NavLink>
+        </li>
+        <li>
           <NavLink to="/categories/eyebrow" className="nav-link" id="eyebrow">
             eyebrow
           </NavLink>
@@ -37,23 +47,13 @@ const Nav = ({ handleChange }) => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/categories/blush" className="nav-link" id="blush">
-            blush
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories/bronzer" className="nav-link" id="bronzer">
-            bronzer
+          <NavLink to="/categories/lipliner" className="nav-link" id="lipliner">
+            lip liner
           </NavLink>
         </li>
         <li>
           <NavLink to="/categories/lipstick" className="nav-link" id="lipstick">
             lipstick
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories/lipliner" className="nav-link" id="lipliner">
-            lip liner
           </NavLink>
         </li>
         <li>
