9179cf653ee925f64ee0602d59b4886437d74340
Nina Brissey <nbrissey@gmail.com>
2021-09-25 17:13:51 -0700

    Add all makeup type categories

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -9,28 +9,65 @@ const Nav = ({ handleChange }) => {
       <img className="nav-logo" src={logo} alt="clean beauty logo" />
       <ul className="nav-links">
         <li>
-          <NavLink to="/eye" className="nav-link" id="eye">
-            eye
+          <NavLink to="/categories/eyebrow" className="nav-link" id="eyebrow">
+            eyebrow
           </NavLink>
         </li>
         <li>
-          <NavLink to="/face" className="nav-link" id="face">
-            face
+          <NavLink to="/categories/eyeliner" className="nav-link" id="eyeliner">
+            eyeliner
           </NavLink>
         </li>
         <li>
-          <NavLink to="/cheek" className="nav-link" id="cheek">
-            cheek
+          <NavLink
+            to="/categories/eyeshadow"
+            className="nav-link"
+            id="eyeshadow"
+          >
+            eyeshadow
           </NavLink>
         </li>
         <li>
-          <NavLink to="/lipstick" className="nav-link" id="lipstick">
+          <NavLink
+            to="/categories/foundation"
+            className="nav-link"
+            id="foundation"
+          >
+            foundation
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories/blush" className="nav-link" id="blush">
+            blush
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories/bronzer" className="nav-link" id="bronzer">
+            bronzer
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories/lipstick" className="nav-link" id="lipstick">
             lipstick
           </NavLink>
         </li>
         <li>
-          <NavLink to="/nail" className="nav-link" id="nail">
-            nail
+          <NavLink to="/categories/lipliner" className="nav-link" id="lipliner">
+            lip liner
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories/mascara" className="nav-link" id="mascara">
+            mascara
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/categories/nailpolish"
+            className="nav-link"
+            id="nailpolish"
+          >
+            nail polish
           </NavLink>
         </li>
       </ul>
