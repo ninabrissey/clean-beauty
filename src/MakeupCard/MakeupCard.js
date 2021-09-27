99219c063e89d095537823a2184d7d79961dd580
Nina Brissey <nbrissey@gmail.com>
2021-09-27 11:31:44 -0700

    Build makeup card

--- src/MakeupCard/MakeupCard.js
+++ src/MakeupCard/MakeupCard.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './MakeupCard.css';
 
-const MakeupCard = () => {
-  return <article></article>;
+const MakeupCard = ({ makeup }) => {
+  const { brand, name, price, image_link, description, product_colors } =
+    makeup;
+
+  let productColorDisplays;
+  if (product_colors.length > 0) {
+    productColorDisplays = product_colors.map((color) => {
+      return (
+        <div
+          style={{
+            background: color.hex_value,
+            borderRadius: '50%',
+            height: '20',
+            width: '20',
+          }}
+          name={color.colour_name}
+        ></div>
+      );
+    });
+  }
+  return (
+    <article>
+      <img src={image_link} alt={name} />
+      <h3>{name}</h3>
+      <p>${price}</p>
+      <p>{description}</p>
+      <div>{productColorDisplays}</div>
+    </article>
+  );
 };
 
 export default MakeupCard;
