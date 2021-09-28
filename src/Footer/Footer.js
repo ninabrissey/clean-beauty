127522ead151b377c40397b73fb579fcb37d40cb
Nina Brissey <nbrissey@gmail.com>
2021-09-27 22:39:03 -0700

    Add alt text to reach 100% accessability

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -12,28 +12,28 @@ const Footer = () => {
   return (
     <div className="tag-links">
       <a href="https://www.usda.gov/topics/organic" target="_blank">
-        <img src={organic} />
+        <img src={organic} alt="organic logo" />
       </a>
       <a href="https://vegan.org/" target="_blank">
-        <img src={vegan} />
+        <img src={vegan} alt="vegan logo" />
       </a>
       <a
         href="https://www.aad.org/public/everyday-care/skin-care-basics/dry/oily-skin"
         target="_blank"
       >
-        <img src={oilFree} />
+        <img src={oilFree} alt="oil free logo" />
       </a>
       <a href="https://www.ewg.org/skindeep/" target="_blank">
-        <img src={ewg} />
+        <img src={ewg} alt="ewg logo" />
       </a>
       <a href="https://www.crueltyfreeinternational.org/" target="_blank">
-        <img src={crueltyFree} />
+        <img src={crueltyFree} alt="cruelty free logo" />
       </a>
       <a
         href="https://www.fda.gov/cosmetics/cosmetics-labeling-claims/alcohol-free"
         target="_blank"
       >
-        <img src={alcoholFree} />
+        <img src={alcoholFree} alt="alcohol free logo" />
       </a>
     </div>
   );
