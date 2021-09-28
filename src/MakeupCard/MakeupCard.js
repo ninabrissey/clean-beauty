a9205101e2addcf3c59edf043d50fbb82768930a
Nina Brissey <nbrissey@gmail.com>
2021-09-27 18:49:00 -0700

    Map color options - move to indiv display

--- src/MakeupCard/MakeupCard.js
+++ src/MakeupCard/MakeupCard.js
@@ -1,33 +1,43 @@
 import React from 'react';
+import alcoholFree from '.././assets/alcohol-free-logo.png';
 import './MakeupCard.css';
 
 const MakeupCard = ({ makeup }) => {
-  const { brand, name, price, image_link, description, product_colors } =
-    makeup;
+  const {
+    brand,
+    name,
+    price,
+    image_link,
+    description,
+    product_colors,
+    tag_list,
+  } = makeup;
 
-  let productColorDisplays;
-  if (product_colors.length > 0) {
-    productColorDisplays = product_colors.map((color) => {
-      return (
-        <div
-          style={{
-            background: color.hex_value,
-            borderRadius: '50%',
-            height: '20',
-            width: '20',
-          }}
-          name={color.colour_name}
-        ></div>
-      );
-    });
-  }
+  let productColorDisplays = product_colors.map((color) => {
+    return (
+      <div
+        style={{
+          background: color.hex_value,
+          borderRadius: '50%',
+          height: '15px',
+          width: '15px',
+          margin: '1px',
+        }}
+        name={color.colour_name}
+      ></div>
+    );
+  });
+
   return (
-    <article className="card">
+    <article className="card :hover ">
       <img className="card-img" src={image_link} alt={name} />
       <h3>{name}</h3>
       {/* <p>${price}</p> */}
       {/* <p>{description}</p> */}
-      <div>{productColorDisplays}</div>
+      {/* <div className="colors">{productColorDisplays}</div> */}
+      {tag_list.includes('alcohol free') && (
+        <img className="tag-img" src={alcoholFree} />
+      )}
     </article>
   );
 };
