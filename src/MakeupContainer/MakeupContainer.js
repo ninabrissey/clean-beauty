2e2ade2efcd9deeb67babad3ce7ee6599607a64d
Nina Brissey <nbrissey@gmail.com>
2021-09-27 22:10:04 -0700

    Add search logic

--- src/MakeupContainer/MakeupContainer.js
+++ src/MakeupContainer/MakeupContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import MakeupCard from '../MakeupCard/MakeupCard';
+import Search from '../Search/Search';
 import './MakeupContainer.css';
 
 const MakeupContainer = ({ id }) => {
   const [makeupByType, setMakeupByType] = useState([]);
+  const [filteredMakeup, setFilteredMakeup] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -19,6 +21,7 @@ const MakeupContainer = ({ id }) => {
           (makeup) => makeup.tag_list.length > 0
         );
         setMakeupByType(cleanMakeup);
+        setFilteredMakeup(cleanMakeup);
         setIsLoading(false);
       } catch (error) {
         setError(error.message);
@@ -27,10 +30,50 @@ const MakeupContainer = ({ id }) => {
     getMakeupByType(id);
   }, [id]);
 
-  const makeupCards = makeupByType.map((makeup) => {
+  const filterMakeup = (searchInput) => {
+    const matchedMakeup = makeupByType.filter((makeup) => {
+      if (makeup.brand !== null) {
+        if (makeup.brand.toLowerCase().includes(searchInput)) {
+          return makeup;
+        }
+      }
+      if (makeup.product_type !== null) {
+        if (makeup.product_type.toLowerCase().includes(searchInput)) {
+          return makeup;
+        }
+      }
+      if (makeup.name !== null) {
+        if (makeup.name.toLowerCase().includes(searchInput)) {
+          return makeup;
+        }
+      }
+      if (makeup.description !== null) {
+        if (makeup.description.toLowerCase().includes(searchInput)) {
+          return makeup;
+        }
+      }
+
+      // if (
+      //   makeup.brand.toLowerCase().includes(searchInput) ||
+      //   makeup.product_type.toLowerCase().includes(searchInput) ||
+      //   makeup.name.toLowerCase().includes(searchInput) ||
+      //   makeup.description.toLowerCase().includes(searchInput)
+      // ) {
+      //   return makeup;
+      // }
+    });
+    setFilteredMakeup(matchedMakeup);
+  };
+
+  const makeupCards = filteredMakeup.map((makeup) => {
     return <MakeupCard key={makeup.category + makeup.id} makeup={makeup} />;
   });
-  return <section className="makeup-container">{makeupCards}</section>;
+  return (
+    <section className="searchAndContainer">
+      <Search filterMakeup={filterMakeup} />
+      <section className="makeup-container">{makeupCards}</section>
+    </section>
+  );
 };
 
 export default MakeupContainer;
